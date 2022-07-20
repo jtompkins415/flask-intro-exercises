6db4d7af448a08247afc20e5f93105640b8fd373
jtompkins415 <john.tompkins415@gmail.com>
2022-07-19 17:30:17 -0700

    Write calc logic

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,37 @@
-# Put your app in here.
+from operations import *
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/add')
+def do_add():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = add(a,b)
+
+    return result
+
+
+@app.route('/sub')
+def do_sub():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = sub(a,b)
+
+    return result
+
+@app.route('/mult')
+def do_sub():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = mult(a,b)
+
+    return result
+
+@app.route('/div')
+def do_sub():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = div(a,b)
+
+    return result
