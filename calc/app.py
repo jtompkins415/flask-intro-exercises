e0476e3ee2351198939cec9cb97f6315e6b02617
jtompkins415 <john.tompkins415@gmail.com>
2022-07-19 17:38:22 -0700

    run 3rd test, change logic based on 3rd test

--- calc/app.py
+++ calc/app.py
@@ -35,3 +35,21 @@ def do_div():
     result = div(a,b)
 
     return str(result)
+
+
+#PART TWO
+
+math_functions = {
+    'add': add,
+    'sub': sub,
+    'mult': mult,
+    'div': div
+}
+
+@app.route('/operations/<funct>')
+def do_math(funct):
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = math_functions[funct](a,b)
+
+    return str(result)
